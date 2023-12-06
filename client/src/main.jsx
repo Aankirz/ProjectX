8cb6e4ac18936d3880866af26ed5496bbc7a6707
anj20 <jenaanuragnarayan@gmail.com>
2023-12-06 10:30:04 +0530

    feat

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
+const app_id = "826333556876797063130367436121014584984601624576";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AnonAadhaarProvider _appId={app_id}>
+      <App />
+    </AnonAadhaarProvider>
+  </React.StrictMode>
+);
